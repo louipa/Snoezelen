99aa12f0afca51e10c326eeb1199edbb627bc070
clementosaure <clementgaliot569@gmail.com>
2024-05-27 14:46:16 +0200

    feat : camera iso

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import { useState } from 'react'
-import './App.css'
-import { Box } from './components/box'
-import { Canvas } from '@react-three/fiber'
+import { useState } from "react";
+import "./App.css";
+import { Box } from "./components/box";
+import { Canvas } from "@react-three/fiber";
 
 function App() {
-
   return (
-    <Canvas>
-    <Box position={[-1.2, 0, 0]} />
-    <ambientLight></ambientLight>
-  </Canvas>
-  )
+    <Canvas orthographic camera={{ zoom: 50, position: [0, 0, 100] }}>
+      <Box position={[0, 0, -5]} />
+      <Box position={[0, 1, -5]} />
+      <ambientLight />
+    </Canvas>
+  );
 }
 
-export default App
+export default App;
