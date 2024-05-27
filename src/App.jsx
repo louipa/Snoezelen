56f317b433afdf9925fc36e52ae60947599b9ab8
clementosaure <clementgaliot569@gmail.com>
2024-05-27 16:40:11 +0200

    feat : burger menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
-import { useState } from "react";
-import "./App.css";
-import { Box } from "./components/box";
-import { Canvas } from "@react-three/fiber";
+import { useState } from 'react';
+import './App.css';
+import { Box } from './components/box';
+import { Canvas } from '@react-three/fiber';
+import Sidebar from './components/sidebar';
 
 function App() {
-  return (
-    <Canvas orthographic camera={{ zoom: 50, position: [0, 0, 100] }}>
-      <Box position={[0, 0, -5]} />
-      <Box position={[0, 1, -5]} />
-      <ambientLight />
-    </Canvas>
-  );
+    return (
+        <Canvas orthographic camera={{ zoom: 50, position: [0, 0, 100] }}>
+            <Box position={[0, 0, -5]} />
+            <Box position={[0, 1, -5]} />
+            <ambientLight />
+        </Canvas>
+    );
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
+import Sidebar from './components/sidebar.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
+        <Sidebar />
         <App />
     </React.StrictMode>
 );
