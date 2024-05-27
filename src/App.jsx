45ff3bd4b9fbb7f85fff6081ffec0802db1ab34f
LouisMDK <louis.painter49@gmail.com>
2024-05-27 12:44:41 +0200

    :building_construction: add react three fiber + vite

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import { Box } from './components/box'
+import { Canvas } from '@react-three/fiber'
+
+function App() {
+
+  return (
+    <Canvas>
+    <Box position={[-1.2, 0, 0]} />
+    <ambientLight></ambientLight>
+  </Canvas>
+  )
+}
+
+export default App
