4487baf1569bacf47a00733881ef1bc6209fa513
antoine otegui <antoine.otegui@soprasteria.com>
2024-05-27 14:44:47 +0200

    udpate eslint

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,3 +1,11 @@
 module.exports = {
-  ...require('gts/.prettierrc.json'),
+    ...require('gts/.prettierrc.json'),
+    tabWidth: 4,
+    semi: true,
+    singleQuote: true,
+    trailingComma: 'none',
+    arrowParens: 'always',
+    bracketSpacing: true,
+    jsxBracketSameLine: false,
+    endOfLine: 'auto'
 };
--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import './App.css';
-import {Box} from './components/box';
-import {Canvas} from '@react-three/fiber';
+import { Box } from './components/box';
+import { Canvas } from '@react-three/fiber';
 
 function App() {
-  return (
-    <Canvas>
-      <Box position={[-1.2, 0, 0]} />
-      <ambientLight> </ambientLight>
-    </Canvas>
-  );
+    return (
+        <Canvas>
+            <Box position={[-1.2, 0, 0]} />
+            <ambientLight> </ambientLight>
+        </Canvas>
+    );
 }
 
 export default App;
