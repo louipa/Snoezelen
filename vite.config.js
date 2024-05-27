7b1e94f2d281416c2f53c032857c1880ba9615d9
Louis Painter <louis.painter49@gmail.com>
2024-05-27 15:00:32 +0200

    :hammer: remove prepare script

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: 'https://l23paint.gitlab-pages.imt-atlantique.fr'
+    base: 'https://l23paint.gitlab-pages.imt-atlantique.fr',
 });
