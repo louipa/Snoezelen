35cde28181ecf63547d8c966848eebc54ae8fd65
Louis Painter <louis.painter49@gmail.com>
2024-05-28 18:09:20 +0200

    :construction: wip for Antoine <3

--- src/pages/NeuralSignal/NeuralSignal.tsx
+++ src/pages/NeuralSignal/NeuralSignal.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import Worm from '../../components/Worm';
 import { Canvas, useThree } from '@react-three/fiber';
+import { randomFieldVectors } from './perlin.js';
 
 import * as THREE from 'three'; // Import the THREE object from the three package
 
@@ -15,6 +16,9 @@ const SetBackgroundColor = ({ color }) => {
 };
 
 export default function SquishCube() {
+    useEffect(() => {
+        console.log(randomFieldVectors(10));
+    }, []);
     return (
         <Canvas orthographic camera={{ zoom: 50, position: [0, 0, 100] }}>
             <SetBackgroundColor color="#222222" />
