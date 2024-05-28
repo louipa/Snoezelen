97eaf26cd702fb1b943160058767a60563a72fcc
Louis Painter <louis.painter49@gmail.com>
2024-05-28 14:36:37 +0200

    :construction: fractales

--- src/pages/Fractales.tsx
+++ src/pages/Fractales.tsx
@@ -14,8 +14,18 @@ function lerp(a: number, b: number, t: number): number {
     return a + (b - a) * t;
 }
 
+function pickHex(color1, color2, weight) {
+    const w1 = weight;
+    const w2 = 1 - w1;
+    return [
+        Math.round(color1[0] * w1 + color2[0] * w2),
+        Math.round(color1[1] * w1 + color2[1] * w2),
+        Math.round(color1[2] * w1 + color2[2] * w2)
+    ];
+}
+
 const Fractales: React.FC<IFractales> = () => {
-    const [radius, setRadius] = useState(4);
+    const radius = 4;
     const nbPoints = 100000;
     const [sides, setSides] = useState(3);
     const [interpolation, setInterpolation] = useState(0.7);
@@ -34,7 +44,8 @@ const Fractales: React.FC<IFractales> = () => {
             );
         }
 
-        const result = new Float32Array(nbPoints * 3);
+        const positions = new Float32Array(nbPoints * 3);
+        const colors = new Float32Array(nbPoints * 3);
         let x = Math.random() * radius;
         let y = Math.random() * radius;
 
@@ -46,12 +57,23 @@ const Fractales: React.FC<IFractales> = () => {
             x = lerp(x, target.x, interpolation);
             y = lerp(y, target.y, interpolation);
 
-            result[i3] = x;
-            result[i3 + 1] = y;
-            result[i3 + 2] = 0;
+            positions[i3] = x;
+            positions[i3 + 1] = y;
+            positions[i3 + 2] = 0;
+
+            const test = pickHex(
+                [200, 0, 0],
+                [0, 0, 100],
+                (Math.abs(x) / 2 + Math.abs(y) / 2) / radius
+            );
+            colors[i3] = test[0] / 255;
+            colors[i3 + 1] = test[1] / 255;
+            colors[i3 + 2] = test[2] / 255;
         }
-        return result;
+        console.log(colors);
+        return [positions, colors];
     }, [radius, sides, nbPoints, interpolation]);
+
     return (
         <>
             <input
@@ -77,8 +99,8 @@ const Fractales: React.FC<IFractales> = () => {
             />
             <Canvas camera={{}} style={{ backgroundColor: 'black' }}>
                 <Polygon radius={radius} sides={sides} />
-                <Points positions={points}>
-                    <pointsMaterial color="red" size={0.02} />
+                <Points positions={points[0]} colors={points[1]}>
+                    <pointsMaterial vertexColors size={0.02} />
                 </Points>
             </Canvas>
         </>
