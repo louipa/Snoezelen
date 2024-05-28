221c6128773067a2d3989a86a3e5ba2b255c1390
Louis Painter <louis.painter49@gmail.com>
2024-05-28 18:44:27 +0200

    :construction: wip pour Antoine

--- src/pages/NeuralSignal/NeuralSignal.tsx
+++ src/pages/NeuralSignal/NeuralSignal.tsx
@@ -4,6 +4,7 @@ import { Canvas, useThree } from '@react-three/fiber';
 import { randomFieldVectors } from './perlin.js';
 
 import * as THREE from 'three'; // Import the THREE object from the three package
+import { Line } from '@react-three/drei';
 
 const SetBackgroundColor = ({ color }) => {
     const { scene } = useThree();
@@ -15,15 +16,27 @@ const SetBackgroundColor = ({ color }) => {
     return null;
 };
 
-export default function SquishCube() {
-    useEffect(() => {
-        console.log(randomFieldVectors(10));
-    }, []);
+export default function NeuralSignal() {
+    const vectors = randomFieldVectors(100);
+    const offset = vectors.length / 2;
+
     return (
-        <Canvas orthographic camera={{ zoom: 50, position: [0, 0, 100] }}>
+        <Canvas orthographic camera={{ zoom: 5 }}>
             <SetBackgroundColor color="#222222" />
             <Worm width={0.05} length={200}></Worm>
-
+            {vectors.map((row, i) =>
+                row.map((vector, j) => (
+                    <Line
+                        key={`${i}-${j}`}
+                        points={[
+                            [i - offset, j - offset, 0],
+                            [i - offset + vector.x, j - offset + vector.y, 0]
+                        ]}
+                        color="white"
+                        lineWidth={1}
+                    />
+                ))
+            )}
             <ambientLight />
         </Canvas>
     );
